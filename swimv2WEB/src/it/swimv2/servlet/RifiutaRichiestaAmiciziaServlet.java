7f2ba692e3027503330412321c5f2294c3f2efc3
Michele <Michele@Laptop>
2013-01-27 04:36:59 +0100

    RifiutaRichiestaAmiciziaServlet

---
+++ swimv2WEB/src/it/swimv2/servlet/RifiutaRichiestaAmiciziaServlet.java
@@ -0,0 +1,38 @@
+package it.swimv2.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RifiutaRichiestaAmiciziaServlet
+ */
+public class RifiutaRichiestaAmiciziaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public RifiutaRichiestaAmiciziaServlet() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
